46dff47c3b7f6e51a5cfc6157de8bfcc3549b19d
筒井 大二 <tsutsui@imac-3.local>
2018-06-22 18:14:27 +0900

    Initial Commit

---
+++ SupportVectorMachine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SupportVectorMachine
+//
+//  Created by 筒井 大二 on 2018/06/22.
+//  Copyright © 2018年 筒井 大二. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
